a4653b8e355e75445c759e5f0885db7b0483626c
Fisht_4 <m2024615840@163.com>
2026-06-01 20:23:16 +0800

    6.1

---
+++ MCCS/include/MCCS.h
@@ -0,0 +1,17 @@
+#ifndef MCCS_H
+#define MCCS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+#include <sys/socket.h>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+
+#endif
---
+++ MCCS/src/main.cpp
@@ -0,0 +1,8 @@
+#include "MCCS.h"
+
+int main() {
+
+    int server_fd, new_socket;
+    
+
+}
